147791435f0f09a1c0ccfc23c0e911b25a65b1d7
Mattias Carlén <mcarlen@gmail.com>
2024-03-20 20:54:32 +0100

    Initial Commit

---
+++ Landmann Smart Temperature/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmann Smart Temperature
+//
+//  Created by Mattias Carlén on 2024-03-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmann Smart Temperature/Landmann_Smart_TemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmann_Smart_TemperatureApp.swift
+//  Landmann Smart Temperature
+//
+//  Created by Mattias Carlén on 2024-03-20.
+//
+
+import SwiftUI
+
+@main
+struct Landmann_Smart_TemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
